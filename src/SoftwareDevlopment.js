19e60b8bbe3b3b57a0e72502a02b9599ed521df4
AnkitO2 <120965263+AnkitO2@users.noreply.github.com>
2023-09-01 20:57:23 +0530

    Website Designing   Page

---
+++ src/SoftwareDevlopment.js
@@ -0,0 +1,183 @@
+import React from 'react'
+import { Row, Col, Card, CardBody, CardTitle, CardText } from 'reactstrap'
+
+export const SoftwareDevlopment = () => {
+    return (
+        <>
+            <Row>
+                <Col>
+                    <img src='https://ml85xkxbvpuq.i.optimole.com/cb:pIqd.8ad3/w:700/h:700/q:mauto/f:avif/https://orbishinfotech.com/wp-content/uploads/2023/06/Best-Software-Company-in-Lucknow.png' />
+                </Col>
+                <Col>
+                    <h6>Best Software Development
+                        Company in Lucknow
+                    </h6>
+                    <p>At  Orbish Infotech, we offer custom software development services
+                        that are tailored to meet the unique needs of businesses. Our expert
+                        team of software developers has extensive experience in building
+                        innovative, reliable, and scalable software solutions for various
+                        industries. Orbish Infotech is the best software development Company
+                        in Lucknow or the Top software development company in
+                        Lucknow. Orbish Infotech, the best software company in Lucknow,
+                        offers top-notch software solutions tailored to your business needs.
+                        offers top-notch software solutions tailored to your business needs
+                        Recognized as one of the top 10 software companies in Lucknow, they
+                        combine expertise, innovation, and customer satisfaction to drive your
+                        success. Regarding software development in Lucknow, Orbish Infotech
+                        stands out as one of the leading companies in the industry. With a
+                        proven track record of delivering high-quality software solutions, we
+                        have earned a reputation as a top 10 best software development
+                        company in Lucknow  .
+                    </p>
+
+                    <p>
+                        We provide top-notch software development services to businesses
+                        across various industries. Our experienced team of developers
+                        leverages the latest tools and technologies to create custom software
+                        solutions tailored to meet the unique needs of our clients. We work
+                        closely with our clients to understand their business goals, challenges,
+                        and requirements, ensuring that the software we develop aligns with
+                        their business objectives and delivers maximum value.
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <img src='https://ml85xkxbvpuq.i.optimole.com/cb:pIqd.8ad3/w:512/h:360/q:mauto/f:avif/https://orbishinfotech.com/wp-content/uploads/2023/04/Custom-software-development-services-in-lucknow.png' />
+                </Col>
+                <Col>
+                    <h6>Why Orbish Infotech as a Best Custom software
+                        development
+                    </h6>
+                    <p>
+                        Software development is at the core of our business at Orbish Infotech. We specialize
+                        in developing custom software solutions that help businesses streamline their
+                        operations, increase efficiency, and drive growth. Our experienced team of developers
+                        leverages the latest tools and technologies to create software solutions that are
+                        tailored to meet the unique needs of our clients.
+                    </p>
+                    <p>
+                        Our software development process follows a comprehensive approach that includes
+                        planning, design, development, testing, and deployment. We use agile development
+                        methodologies to ensure that the software is delivered on time and within budget, with
+                        a focus on quality and reliability.
+                    </p>
+                    <p>
+                        We provide end-to-end software development solutions that are tailored to meet the
+                        specific needs of our clients. Our team works closely with clients to understand their
+                        requirements and develop software applications that deliver exceptional user
+                        experience.
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle> Custom software solutions</CardTitle>
+                            <CardText>
+                                We specialize in creating custom software solutions tailored to meet the unique needs of our clients. Our expert team of developers works closely with clients to understand their business objectives, requirements, and challenges, ensuring that the software we develop aligns with their goals and delivers maximum value.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>Expert development team</CardTitle>
+                            <CardText>
+                                Our team of software developers is highly skilled and experienced in the latest tools and technologies, allowing us to create innovative and reliable software solutions that are secure, scalable, and easy to use. We leverage agile development methodologies to ensure that the software is delivered on time and within budget, with a focus on quality and reliability.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>Responsive web applications</CardTitle>
+                            <CardText>
+                                We create responsive and scalable web applications that are optimized for various devices, platforms, and browsers. Our web applications are designed to enhance user engagement and drive business growth, with a focus on delivering exceptional user experience. Our web applications are designed to enhance user engagement and drive business growth.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>
+                                Mobile application development
+                            </CardTitle>
+                            <CardText>
+                                Our mobile application development services help businesses create mobile applications that are user-friendly, secure, and engaging. We develop mobile applications for iOS, Android, and other platforms, ensuring that the app works seamlessly across different devices.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>
+                                Enterprise-grade software
+                                solutions
+                            </CardTitle>
+                            <CardText>
+                                We develop enterprise-grade software solutions that enable businesses to streamline their operations, increase efficiency, and drive growth. Our enterprise software solutions are designed to be scalable, secure, and reliable, with a focus on delivering exceptional user experience.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>
+                                Comprehensive software
+                                development process
+                            </CardTitle>
+                            <CardText>
+                                Our software development process follows a comprehensive approach that includes planning, design, development, testing, and deployment. We use agile development methodologies to ensure that the software is delivered on time and within budget, with a focus on quality and reliability.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <h6>Are you looking for the best software company in Lucknow?</h6>
+                <p>Look no further than Orbish Infotech. As one of the top software development firms in Lucknow, we are known for our excellence in delivering cutting-edge
+                    software solutions. With a strong presence in the city, valour Infotech is your trusted partner for all your software needs.
+                </p>
+                <p>
+                    Our team of skilled software developers and engineers is dedicated to providing the highest quality services. As a top software development company in
+                    Lucknow, we have built a solid reputation for delivering innovative and customized software solutions to our clients. We cater to various industries,
+                    including real estate, and MLM software, and have successfully developed real estate software solutions that streamline operations and enhance efficiency.
+                </p>
+                <p>
+                    Our software company is at the forefront of technological advancements. We pride ourselves on being a leading software and web development company in
+                    Lucknow, constantly exploring the latest technologies and utilizing the best development practices. Our goal is to develop software that not only meets but
+                    exceeds your expectations. We offer software services in Gomti Nagar, Vikas Nagar, Hazratganj, etc. a prime area in Lucknow.
+                </p>
+            </Row>
+            <Row>
+                <h6>Why Choose Orbish Infotech as the Best Software Company?</h6>
+                <p>At Orbish Infotech, client satisfaction is our top priority. We understand that every business has unique requirements, and we tailor our services
+                    accordingly. Whether you are a startup or an established enterprise, we have the expertise to cater to your specific needs. Our comprehensive software
+                    development services cover everything from conceptualization and design to development, testing, and support.
+                </p>
+                <p>
+                    As one of the best software companies in Lucknow, we offer a wide range of services to meet your diverse software needs. Our team of experienced
+                    developers specializes in web and mobile app development, software consulting, software testing, and more. We have successfully completed numerous
+                    projects for clients in Lucknow and beyond, helping them achieve their business goals.
+                </p>
+                <p>
+                    Regarding IT companies in Lucknow, Orbish Infotech stands out as a leader. Our dedication to excellence, commitment to innovation, and customer-centric
+                    approach make us the top choice for software development in Lucknow. Join hands with us and experience the transformative power of technology for your
+                    business.
+                </p>
+                <p>Contact us today to discuss your software development requirements and discover why Orbish Infotech is the best software development company in
+                    Lucknow.
+                </p>
+            </Row>
+        </>
+    )
+}
---
+++ src/WebsiteDesigning.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import { Button, Card, CardBody, CardText, CardTitle, Col, Row } from 'reactstrap'
+
+export const WebsiteDesigning = () => {
+    return (
+        <>
+            <Row>
+                <Col>
+                    <img src='https://ml85xkxbvpuq.i.optimole.com/cb:pIqd.8ad3/w:873/h:563/q:mauto/f:avif/https://orbishinfotech.com/wp-content/uploads/2023/04/best-website-designing-company-in-lucknow-orbish-infotech.png' />
+                </Col>
+                <Col>
+                    <h6>Best Web Designing Company in Lucknow</h6>
+                    <p>
+                        At valour Infotech, we understand the importance of a well-designed website for
+                        businesses of all sizes. A website is often the first point of contact between a business
+                        and its potential customers. A well-designed website can create a great first impression,
+                        increase user engagement, and drive business growth. We are in Top Web Designing
+                        Company in Lucknow.
+                    </p>
+                    <p>
+                        We specialize in providing end-to-end web designing solutions that are tailored to meet
+                        the unique needs of our clients. Our experienced team of designers works closely with
+                        clients to understand their requirements and develop websites that are visually
+                        appealing, user-friendly, and optimized for search engines. We are the best Web
+                        appealing, user-friendly, and optimized for search engines. We are the best Web
+                        Designing Company in Lucknow or the best website designing companies in Lucknow.
+                    </p>
+                    <p>
+                        Our web designing process follows a comprehensive approach that includes planning,
+                        design, development, testing, and deployment. We use the latest tools and technologies
+                        to create websites that are responsive, scalable, and optimized for various devices,
+                        platforms, and browsers.
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <h6>
+                    Looking for the best web designing company in Lucknow? Look no further than
+                    Orbish Infotech. Our experienced team of web designers provides end-to-end web
+                    designing solutions that are tailored to meet the unique needs of your business.
+                </h6>
+                <Button>Enquire Now</Button>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>Custom web designing</CardTitle>
+                            <CardText>
+                                We provide custom web designing solutions that are tailored to meet the specific needs of our clients. Our team works closely with clients to understand their requirements and create websites that deliver exceptional user experience.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>Responsive web designing</CardTitle>
+                            <CardText>
+                                We create responsive websites that are optimized for various devices, platforms, and browsers. Our responsive web designing solutions are designed to enhance user engagement and drive business growth.
+                            </CardText>
+                        </CardBody>
+                    </Card></Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>
+                                E-commerce web designing
+                            </CardTitle>
+                            <CardText>
+                                We provide e-commerce web designing solutions that help businesses sell their products and services online. Our e-commerce websites are designed to be secure, user-friendly, and optimized for search engines.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>
+                                Website redesigning
+                            </CardTitle>
+                            <CardText>
+                                We provide website redesigning solutions that help businesses revamp their existing websites to create a fresh and modern look. Our website redesigning solutions are designed to improve user experience, enhance functionality, and increase online visibility.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <h6>Why Orbish Infotech?</h6>
+                <p>At Orbish Infotech, we understand that a well-designed website is crucial for your online presence and business success. That’s why our website developer
+                    in Lucknow employs the latest technologies and industry best practices to ensure that your website stands out from the competition.
+                </p>
+                <p>
+                    In addition to website design, we also offer graphic design services in Lucknow. Our experienced graphic designer in Lucknow can create captivating logos,
+                    banners, and other visuals that enhance your brand identity and leave a lasting impact on your audience.
+                </p>
+                <p>
+                    As a reputable website design company in Lucknow, we prioritize client satisfaction and deliver projects within agreed timelines. Our commitment to
+                    excellence has earned us the reputation of being the best website designing company in Lucknow.
+                </p>
+                <p>
+                    Contact us today to inquire about our web designing course fees in Lucknow.
+                </p>
+                <p>
+                    When it comes to website designing in Lucknow, Orbish Infotech is your trusted partner. We are expertise in website design, web development, and graphic
+                    design makes us the go-to choice for all your web design needs in Lucknow. Experience the difference between professional website design and contact us
+                    for your website design services in Lucknow. Let us help you establish a strong online presence with our unmatched web design services in Lucknow.
+                </p>
+            </Row>
+        </>
+    )
+}
