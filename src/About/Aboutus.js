d6bd5fc3828b5cedf3ed3baf43b54d086c32d911
AnkitO2 <120965263+AnkitO2@users.noreply.github.com>
2024-01-05 00:20:10 +0530

    To add Styles

--- src/About/Aboutus.js
+++ src/About/Aboutus.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Col, Container, Row } from "reactstrap";
 import "./Aboutus.css";
 import { Footerbaar } from "../Footer/Footerbaar";
+import logo1 from "../logo/logoOne.jpg";
 const Aboutus = () => {
   return (
     <>
-      <Container className="mt-3 ">
+      <Container className="mt-3">
         <Row>
           <Col lg={6} md={6} sm={12}>
             <h2 className="">About Ou - Valour Infotech</h2>
             <p
-              className="Aboutus1
+              className="AboutUsText
 
 "
             >
@@ -23,7 +24,7 @@ const Aboutus = () => {
               mobile application development, software testing, software
               maintenance and support.
             </p>
-            <p className="Aboutus1">
+            <p className="AboutUsText">
               Valour Infotech’s team may be comprised of experienced
               professionals proficient in various technical and programming
               languages. They work closely with customers to understand their
@@ -37,10 +38,8 @@ const Aboutus = () => {
           </Col>
           <Col lg={6} md={6} sm={12}>
             <div className="d-flex justify-content-center">
-              <img
-                src="https://ml85xkxbvpuq.i.optimole.com/cb:pIqd.8ad3/w:500/h:500/q:mauto/f:avif/https://orbishinfotech.com/wp-content/uploads/2023/06/how-orbish-infotech-work-software-field.webp"
-                alt="Error"
-              />
+              <img src={logo1} alt="Error" className="Img1"/>
+              
             </div>
           </Col>
         </Row>
@@ -55,7 +54,7 @@ const Aboutus = () => {
           </Col>
           <Col lg={6} md={6} sm={12}>
             <h2>#About Valour Infotech </h2>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               valour Infotech is a software company based in Lucknow, Uttar
               Pradesh, specializing in providing a wide range of software
               solutions to cater to diverse business needs. With a focus on
@@ -64,7 +63,7 @@ const Aboutus = () => {
               designing, Web Development, App Development, Graphic Designing &
               Digital Marketing.
             </p>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               valour Infotech is a comprehensive software solutions provider
               that aims to cater to a wide range of business requirements. With
               their expertise and experience in the software industry, they
@@ -77,14 +76,14 @@ const Aboutus = () => {
         <Row>
           <Col lg={6} md={6} sm={12}>
             <h2>Our Mission</h2>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               At valour Infotech, our mission is to empower businesses with
               innovative and reliable software solutions that enable them to
               achieve their goals. valour Infotech team is committed to
               providing exceptional customer service and building strong
               relationships with our clients.
             </p>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               #1 valour team experts are passionate about technology and strive
               to stay at the forefront of the industry. We accept that
               innovation ought to be utilised to drive business development, and
@@ -92,7 +91,7 @@ const Aboutus = () => {
               extraordinary necessities and make customised arrangements that
               assist them with accomplishing their targets.
             </p>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               valour Infotech are dedicated to delivering quality customised
               services that exceed our clients’ expectations. We esteem
               genuineness, respectability, and straightforwardness, and we
@@ -120,18 +119,18 @@ const Aboutus = () => {
           </Col>
           <Col lg={6} md={6} sm={12}>
             <h2>Our Vission</h2>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               At valour Infotech, our vision is to become a global leader in
               software development, known for our exceptional quality of work,
               commitment to our clients, and innovative solutions that help
               businesses thrive.
             </p>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               valour Infotech believes in utilising the latest technologies and
               tools to create customised software solutions that are reliable,
               secure, and scalable.
             </p>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               Our fundamental objective is to be a trusted accomplice to our
               clients, assisting them with accomplishing their business targets
               through programming arrangements that are custom-made to their
@@ -142,34 +141,34 @@ const Aboutus = () => {
         <Row>
           <Col lg={6} md={6} sm={12}>
             <h2>Our Values</h2>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               Valour Infotech Lucknow believes that our core values guide our
               behaviour, decisions, and interactions with our clients and team
               members. Our core values are as follow :
             </p>
             <h5>Customer Satisfaction:</h5>
 
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               Our main aim is to understand their client needs and create
               customised software solutions that meet their specific
               requirements.
             </p>
             <h5>Excellence:</h5>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               We make progress toward greatness in all that we do. We are
               passionate about the new technology and our engagement models are
               flexible, secure and custom defined based on specific business
               needs of our customers.
             </p>
             <h5>Integrity:</h5>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               valour Infotech values honesty, integrity, and transparency in all
               our interactions with our clients and team members. We believe
               that building trust is crucial to establishing long-lasting
               relationships.
             </p>
             <h5>Innovation:</h5>
-            <p className="text-dark-emphasis">
+            <p className="AboutUsText">
               valour Infotech works with small businesses and startups,
               government agencies and large private sectors to build
               people-centred tech solutions that use the full capabilities of
--- src/Home/Scrollbaar.js
+++ src/Home/Scrollbaar.js
@@ -5,6 +5,7 @@ import { Carousel, CarouselControl, CarouselIndicators, CarouselCaption, Carouse
 import './Scrollbaar.css';
 const items = [
     {
+        
         src: 'https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
         key: 1,
 
