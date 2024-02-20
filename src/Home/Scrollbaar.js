7d83f17240446b07e5479b61e3be12065af92b05
AnkitO2 <120965263+AnkitO2@users.noreply.github.com>
2024-02-20 13:09:06 +0530

    Add Offline Photos

--- src/Home/Scrollbaar.js
+++ src/Home/Scrollbaar.js
@@ -6,52 +6,51 @@ import './Scrollbaar.css';
 const items = [
     {
         
-        src: 'https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src:"/images/coding1.jpg",
         key: 1,
 
     },
     {
-        src: 'https://images.unsplash.com/photo-1531973576160-7125cd663d86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c29mdHdhcmUlMjBDb21wYW55fGVufDB8fDB8fHww&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 2,
 
     },
     {
-        src: 'https://images.unsplash.com/photo-1573167507387-6b4b98cb7c13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNvZnR3YXJlJTIwQ29tcGFueXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: "",
         key: 3,
     },
     {
-        src: 'https://images.unsplash.com/photo-1571826915679-c3568eab4be0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 4,
     },
     {
-        src: 'https://images.unsplash.com/photo-1444653389962-8149286c578a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 5,
     },
     {
-        src: 'https://images.unsplash.com/photo-1557186814-b29c2b31d57c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
-
+        src: '',
         key: 6,
     },
     {
-        src: 'https://images.unsplash.com/photo-1590650486895-79681b6f26a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 7,
     },
     {
-        src: 'https://images.unsplash.com/photo-1556565681-306458ef93cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 8,
 
     },
     {
-        src: 'https://images.unsplash.com/photo-1542744095-fcf48d80b0fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 9,
     },
     {
-        src: 'https://images.unsplash.com/flagged/photo-1551135049-83f3419ef05c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 10,
 
     },
     {
-        src: 'https://images.unsplash.com/photo-1549045345-058277380fc3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1550&h=600',
+        src: '',
         key: 11,
     },
 ];
@@ -74,7 +73,9 @@ export const Scrollbaar = (args) => {
     };
     const slides = items.map((item) => {
         return (
+            
             <CarouselItem
+            className="custom-tag"
                 onExiting={() => setAnimating(true)}
                 onExited={() => setAnimating(false)}
                 key={item.src}
@@ -95,6 +96,13 @@ export const Scrollbaar = (args) => {
 
             <Row>
                 <Col lg={12} md={12} sm={12}>
+                <style>
+           {`.custom-tag {
+              max-width: 100%;
+              height: 500px;
+              background: black;
+            }`}
+      </style>
                     <Carousel
                         activeIndex={activeIndex}
                         next={next}
